986db66279e399beb075c7812ef1e0795267e085
Ernesto Herrera <ernestohg2002@gmail.com>
2025-12-10 01:49:41 -0500

    c) - h) - f)

--- test.py
+++ test.py
@@ -35,8 +35,9 @@ GROUP BY rcc.IDPub, pub.Texto, usu.Nombre"""
 #3-c)
 def favorite_plants():
     query = """
-    Select
-
+    Select gus.IDProd, COUNT(*) AS Likes
+    FROM Gustar gus
+    GROUP BY gus.IDProd
     """
     return pd.read_sql(query, connection)
 
@@ -67,22 +68,60 @@ def most_popular_post():
     JOIN Reaccionar rcc ON pub.IDPub = rcc.IDPub
     GROUP BY pub.IDPub
     HAVING COUNT(CASE
-                    WHEN rcc.Tipo in ('Me encanta', 'Me gusta', 'Me asombra', 'Me divierte') THEN 1 END) >
+                    WHEN rcc.Tipo IN ('Me encanta', 'Me gusta', 'Me asombra', 'Me divierte') THEN 1 END) >
             COUNT(CASE
-                    WHEN rcc.Tipo in ('Me enoja', 'Me entristece') THEN 1 END)
+                    WHEN rcc.Tipo IN ('Me enoja', 'Me entristece') THEN 1 END)
     """
     return pd.read_sql(query,connection)
 
 #3-f)
-def se_me_acabo_el_ingles():
+def Contribuciones_constantes():#revisar por si acaso
     query = """
     SELECT plt.NombreComun
     FROM Planta plt
     JOIN Contribucion as ctr ON plt.IDProd = ctr.IDProd
-    WHERE
+    JOIN Contribucion as octr ON octr.IDU = ctr.IDU
+    WHERE (DATE_FORMAT(ctr.Fecha, '%Y-%m-01') = DATE_ADD(DATE_FORMAT(octr.Fecha, '%Y-%m-01'), INTERVAL 1 MONTH)
+    OR DATE_FORMAT(octr.Fecha, '%Y-%m-01') = DATE_ADD(DATE_FORMAT(ctr.Fecha, '%Y-%m-01'), INTERVAL 1 MONTH))
+    AND ctr.IDProd = octr.IDProd
     """
     return pd.read_sql(query, connection)
 
+#3-h)
+def Distribucion_de_Edades():#no tiene en cuenta los meses, por si cumplio anos o no
+    query = """
+    SELECT
+    (CASE
+        WHEN  YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) < 11 THEN "wtf"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 11 AND 20 THEN "11-20"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 21 AND 30 THEN "21-30"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 31 AND 40 THEN "31-40"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 41 AND 50 THEN "41-50"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 51 AND 60 THEN "51-60"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 61 AND 70 THEN "61-70"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 71 AND 80 THEN "71-80"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 81 AND 90 THEN "81-90"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) BETWEEN 91 AND 100 THEN "91-100"
+        WHEN YEAR(CURDATE()) - YEAR(usu.FechaDeNacimiento) > 100 THEN "en mis tiempos..."
+    ELSE "revisate eso" 
+    END) AS Rango_de_Edad,
+    COUNT(*) AS Cant_de_Usuarios,
+    (COUNT(*) * 100 / (SELECT COUNT(*) FROM Usuario)) AS por_ciento
+    FROM Usuario usu
+    GROUP BY Rango_de_Edad
+    ORDER BY Rango_de_Edad
+    """
+    return pd.read_sql(query, connection)
+
+#3-l)
+def Compras_Contradictorias():
+    query = """
+    SELECT
+    FROM GUSTAR gus
+    JOIN Usuario usu ON gus.IDU = usu.IDU
+    JOIN Planta plt ON plt.IDProd = gus.IDProd
+    """
+    return pd.read_sql(query, connection)
 
 #build for query
 def change():
@@ -109,10 +148,10 @@ WHERE
 ORDER BY 
     TABLE_NAME, ORDINAL_POSITION;"""
 #print(pd.read_sql(candela, connection)) #tester
-#print(pd.read_sql("Select ctr.Tipo from Reaccionar ctr Group By ctr.Tipo ", connection)) #tester
+#print(pd.read_sql("Select * from Producto", connection)) #tester
 
 print(se_me_acabo_el_ingles())
-=======
+
 # cursor.execute("SELECT * FROM Usuario LIMIT 5")
 
 query = """
